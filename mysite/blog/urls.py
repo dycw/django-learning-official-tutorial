847626b135e883d2c114b1431346864eba226b1c
Derek Wan <d.wan@icloud.com>
2020-11-14 23:34:25 +0800

    Use PostListView class

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,13 +3,13 @@ from __future__ import annotations
 from django.urls import path
 
 from .views import post_detail
-from .views import post_list
+from .views import PostListView
 
 
 app_name = "blog"
 urlpatterns = [
     # post views
-    path("", post_list, name="post_list"),
+    path("", PostListView.as_view(), name="post_list"),
     path(
         "<int:year>/<int:month>/<int:day>/<slug:post>/",
         post_detail,
