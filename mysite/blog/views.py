41510dbf2fa13f787354d62fe5dd724572ad1b8f
Derek Wan <d.wan@icloud.com>
2020-11-15 00:35:33 +0800

    Add EmailPostForm

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -9,6 +9,7 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.views.generic import ListView
 
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -57,4 +58,26 @@ def post_detail(
         request,
         "blog/post/detail.html",
         {"post": post},
+    )
+
+
+def post_share(request: WSGIRequest, post_id: int) -> HttpResponse:
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status="published")
+    if request.method == "POST":
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            form.cleaned_data
+            # ... send email
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        "blog/post/share.html",
+        {
+            "post": post,
+            "form": form,
+        },
     )
