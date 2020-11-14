9f55d901ed2342f5289d3069efdf505713d9f515
Derek Wan <d.wan@icloud.com>
2020-11-14 08:08:48 +0800

    Add homepage.html in urls.py

--- coffeehouse/coffeehouse/urls.py
+++ coffeehouse/coffeehouse/urls.py
@@ -17,7 +17,9 @@ from __future__ import annotations
 
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name="homepage.html")),
 ]
