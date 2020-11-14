d9e2c5bb77a0a693778dea779c299d57fc1d5ba9
Derek Wan <d.wan@icloud.com>
2020-11-14 13:48:12 +0800

    Add post_list, post_detail

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,39 @@
 from __future__ import annotations
 
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request: WSGIRequest) -> HttpResponse:
+    posts = Post.published.all()
+    return render(
+        request,
+        "blog/post/list.html",
+        {"posts": posts},
+    )
+
+
+def post_detail(
+    request: WSGIRequest,
+    year: int,
+    month: int,
+    day: int,
+    post: str,
+) -> HttpResponse:
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status="published",
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
+    return render(
+        request,
+        "blog/post/detail.html",
+        {"post": post},
+    )
