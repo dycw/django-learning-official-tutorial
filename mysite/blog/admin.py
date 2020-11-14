22caecccf067f66a02b07abeeea2d758baedcaa2
Derek Wan <d.wan@icloud.com>
2020-11-14 11:54:24 +0800

    Replace with a PostAdmin(admin.ModelAdmin)

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,8 +1,17 @@
 from __future__ import annotations
 
-from django.contrib.admin import site
+from django.contrib import admin
+from django.contrib.admin import register
 
 from .models import Post
 
 
-site.register(Post)
+@register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "author", "publish", "status")
+    list_filter = ("status", "created", "publish", "author")
+    search_fields = ("title", "body")
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ("author",)
+    date_hierarchy = "publish"
+    ordering = ("status", "publish")
