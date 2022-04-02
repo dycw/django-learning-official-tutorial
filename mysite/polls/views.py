bc52c1fb76907aba2469fd84a7ad91ecdc8126ac
Derek Wan <d.wan@icloud.com>
2022-04-02 17:22:53 +0900

    wip

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,29 +1,29 @@
 from beartype import beartype
-from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
+from django.http.request import HttpRequest
 from django.shortcuts import render
 from polls.models import Question
 
 
 @beartype
-def index(request: WSGIRequest) -> HttpResponse:
+def index(request: HttpRequest) -> HttpResponse:
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 
 
 @beartype
-def detail(_: WSGIRequest, question_id: int) -> HttpResponse:
+def detail(_: HttpRequest, question_id: int) -> HttpResponse:
     return HttpResponse(f"You're looking at question {question_id}.")
 
 
 @beartype
-def results(_: WSGIRequest, question_id: int) -> HttpResponse:
+def results(_: HttpRequest, question_id: int) -> HttpResponse:
     return HttpResponse(
         f"You're looking at the results of question {question_id}."
     )
 
 
 @beartype
-def vote(_: WSGIRequest, question_id: int) -> HttpResponse:
+def vote(_: HttpRequest, question_id: int) -> HttpResponse:
     return HttpResponse(f"You're voting on question {question_id}.")
