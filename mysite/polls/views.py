54da0fe1bf51bf116e979648214f85420e35e99d
Derek Wan <d.wan@icloud.com>
2022-04-02 16:54:17 +0900

    Add

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,3 +6,20 @@ from django.http import HttpResponse
 @beartype
 def index(_: WSGIRequest) -> HttpResponse:
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+@beartype
+def detail(_: WSGIRequest, question_id: int) -> HttpResponse:
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+@beartype
+def results(_: WSGIRequest, question_id: int) -> HttpResponse:
+    return HttpResponse(
+        f"You're looking at the results of question {question_id}."
+    )
+
+
+@beartype
+def vote(_: WSGIRequest, question_id: int) -> HttpResponse:
+    return HttpResponse(f"You're voting on question {question_id}")
