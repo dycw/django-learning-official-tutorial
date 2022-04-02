480f76b7b8132a153f40ffe019dea1c5a6c236a2
Derek Wan <d.wan@icloud.com>
2022-04-02 16:14:22 +0900

    Finish part 2

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,20 +1,34 @@
 import datetime as dt
 from typing import cast
 
+from beartype import beartype
 from django.db.models import CASCADE
 from django.db.models import CharField
 from django.db.models import DateTimeField
 from django.db.models import ForeignKey
 from django.db.models import IntegerField
 from django.db.models import Model
+from django.utils.timezone import now
 
 
 class Question(Model):  # type: ignore
     question_text = cast(str, CharField(max_length=200))
     pub_date = cast(dt.datetime, DateTimeField("date published"))
 
+    @beartype
+    def __str__(self) -> str:
+        return self.question_text
+
+    @beartype
+    def was_published_recently(self) -> bool:
+        return self.pub_date >= now() - dt.timedelta(days=1)
+
 
 class Choice(Model):  # type: ignore
     question = cast(Question, ForeignKey(Question, on_delete=CASCADE))
     choice_text = cast(str, CharField(max_length=200))
     votes = cast(int, IntegerField(default=0))
+
+    @beartype
+    def __str__(self) -> str:
+        return self.choice_text
