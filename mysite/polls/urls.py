36f4e614aaa1e9b8df05e515c7fe6327f92be77f
Derek Wan <d.wan@icloud.com>
2022-04-04 09:09:23 +0900

    Fix

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from polls.views import detail
-from polls.views import index
-from polls.views import results
+from polls.views import DetailView
+from polls.views import IndexView
+from polls.views import ResultsView
 from polls.views import vote
 
 
 app_name = "polls"
 urlpatterns = [
-    path("", index, name="index"),
-    path("<int:question_id>/", detail, name="detail"),
-    path("<int:question_id>/results/", results, name="results"),
+    path("", IndexView.as_view(), name="index"),
+    path("<int:pk>/", DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", vote, name="vote"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,38 +1,51 @@
+from typing import TYPE_CHECKING
+
 from beartype import beartype
+from django.db.models.query import QuerySet
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.urls import reverse
+from django.views.generic import DetailView as _DetailView
+from django.views.generic import ListView as _ListView
 from polls.models import Choice
 from polls.models import Question
 
 
-@beartype
-def index(request: HttpRequest) -> HttpResponse:
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+if TYPE_CHECKING:
+    ListViewQuestion = _ListView[Question]
+    DetailViewQuestion = _DetailView[Question]
+else:
+    ListViewQuestion = _ListView
+    DetailViewQuestion = _DetailView
 
 
-@beartype
-def detail(request: HttpRequest, question_id: int) -> HttpResponse:
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class IndexView(ListViewQuestion):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    @beartype
+    def get_queryset(self) -> QuerySet[Question]:
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-@beartype
-def results(request: HttpRequest, question_id: int) -> HttpResponse:
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class DetailView(DetailViewQuestion):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(DetailViewQuestion):
+    model = Question
+    template_name = "polls/results.html"
 
 
 @beartype
 def vote(request: HttpRequest, question_id: int) -> HttpResponse:
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice: Choice = question.choice_set.get(
+        selected_choice: Choice = question.choice_set.get(  # type: ignore
             pk=request.POST["choice"]
         )
     except (KeyError, Choice.DoesNotExist):
