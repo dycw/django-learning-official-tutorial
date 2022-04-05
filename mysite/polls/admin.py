30b6b348790dd6a4474612d178d21d67a346df64
Derek Wan <d.wan@icloud.com>
2022-04-05 21:02:10 +0900

    Add

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,18 +1,20 @@
 from typing import TYPE_CHECKING
 from typing import Any
 
-from django.contrib import admin
 from django.contrib.admin import ModelAdmin as _ModelAdmin
 from django.contrib.admin import StackedInline as _StackedInline
+from django.contrib.admin import TabularInline as _TabularInline
+from django.contrib.admin import site
 from polls.models import Choice
 from polls.models import Question
 
 
 ModelAdmin = _ModelAdmin[Any] if TYPE_CHECKING else _ModelAdmin
 StackedInline = _StackedInline[Any] if TYPE_CHECKING else _StackedInline
+TabularInline = _TabularInline[Any] if TYPE_CHECKING else _TabularInline
 
 
-class ChoiceInline(StackedInline):
+class ChoiceInline(TabularInline):
     model = Choice
     extra = 3
 
@@ -23,6 +25,9 @@ class QuestionAdmin(ModelAdmin):
         ("Date information", {"fields": ["pub_date"]}),
     ]
     inlines = [ChoiceInline]
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
 
-admin.site.register(Question, QuestionAdmin)
+site.register(Question, QuestionAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,7 +1,11 @@
 import datetime as dt
+from typing import Any
+from typing import Callable
+from typing import TypeVar
 from typing import cast
 
 from beartype import beartype
+from django.contrib.admin import display as _display
 from django.db.models import CASCADE
 from django.db.models import CharField
 from django.db.models import DateTimeField
@@ -9,6 +13,10 @@ from django.db.models import ForeignKey
 from django.db.models import IntegerField
 from django.db.models import Model
 from django.utils.timezone import now
+
+
+_TCallable = TypeVar("_TCallable", bound=Callable[..., Any])
+display = cast(Callable[..., Callable[[_TCallable], _TCallable]], _display)
 
 
 class Question(Model):
@@ -20,6 +28,9 @@ class Question(Model):
         return self.question_text
 
     @beartype
+    @display(
+        boolean=True, ordering="pub_date", description="Published recently?"
+    )
     def was_published_recently(self) -> bool:
         dt_now = now()
         return dt_now - dt.timedelta(days=1) <= self.pub_date <= dt_now
